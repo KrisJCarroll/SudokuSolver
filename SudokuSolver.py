a69dd69f949bf135c43bfb19bd8903d15b917ae4
Kristopher Carroll <kjcarroll@alaska.edu>
2019-11-14 15:26:54 -0900

    fixed file opening errors

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,5 +1,6 @@
 from SudokuCell import SudokuCell
 import csv
+import os
 
 class SudokuSolver:
     digits = "123456789" # used for possible values of cells and column numbers
@@ -7,12 +8,20 @@ class SudokuSolver:
     cols = digits
 
     def __init__(self, file):
-        self.cells = [row+col for row in rows for col in cols]
+
+        self.cells = [row+col for row in SudokuSolver.rows for col in SudokuSolver.cols]
         with open(file) as f:
             csv_reader = csv.reader(f)
             values = []
+            for line in csv_reader:
+                for item in line:
+                    values.append(item)
+            print(values)
 
         
 
 class Main:
     print("Hello world.")
+    path = os.path.dirname(__file__)
+    rel_path = 'ExtremeDifficultyTestSudokus/17-1.txt'
+    solver = SudokuSolver(os.path.join(path, rel_path))
